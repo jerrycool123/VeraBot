2b6490fbd98bd565c37516ad08b79660f0965dfa
jerrycool123 <jerrycool71534@gmail.com>
2022-11-29 02:51:56 +0800

    chore: update debian.sh default agree

--- debian.sh
+++ debian.sh
@@ -2,7 +2,7 @@
 
 # Insall Python 3.10.6
 apt-get update && apt-get upgrade -y
-apt-get install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
 wget https://www.python.org/ftp/python/3.10.6/Python-3.10.6.tgz
 tar -xf Python-3.10.6.tgz
 cd Python-3.10.6/
@@ -11,7 +11,7 @@ make -j $(nproc)
 make altinstall
 
 # Install pip
-apt install python3-pip
+apt install -y python3-pip
 
 # Install tesserocr dependencies
-apt install tesseract-ocr libtesseract-dev libleptonica-dev pkg-config
+apt install -y tesseract-ocr libtesseract-dev libleptonica-dev pkg-config
