e42bb6babe17640f0c8f8bb13a8d460cf99a6ec1
jerrycool123 <jerrycool71534@gmail.com>
2022-11-29 02:46:17 +0800

    feat: add debian docker setup script

--- debian.sh
+++ debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Insall Python 3.10.6
-apt-get install && apt-get upgrade -y
+apt-get update && apt-get upgrade -y
 apt-get install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
 wget https://www.python.org/ftp/python/3.10.6/Python-3.10.6.tgz
 tar -xf Python-3.10.6.tgz
